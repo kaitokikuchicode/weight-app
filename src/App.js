79e49d673034719673c7081d8543b0812b7764a7
kikuchinokaito <kaikai-19990911goal@docomo.ne.jp>
2019-04-07 14:14:07 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight_input: "",
+      weight_list: []
+    };
+  }
+  //It doesn't work
+  /*componentDidMount() {
+    const local_list = [];
+    local_list = localStorage.getItem("local_list");
+    if (local_list !== null) {
+      local_list.split(",");
+      this.setState({
+        weight_list: local_list.toString()
+      });
+    }
+
+    window.addEventListener("beforeunload", () => {
+      localStorage.setItem("local_list", this.state.weight_list);
+    });
+  }*/
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Your Weight</h2>
+        <input onChange={this.input_weight} value={this.state.weight_input} />
+        <br />
+        <button onClick={this.click_button}>submit</button>
+        <br />
+        <div>
+          {this.state.weight_list.map((list, index) => (
+            <li key={index}>
+              {list}
+              <button onClick={() => this.delete(index)}>delete</button>
+            </li>
+          ))}
+        </div>
       </div>
     );
   }
+  input_weight = e => {
+    this.setState({
+      weight_input: e.target.value
+    });
+  };
+
+  click_button = () => {
+    const y = new Date().getFullYear();
+    const m = new Date().getMonth() + 1;
+    const d = new Date().getDate();
+    const date = y + "/" + m + "/" + d;
+
+    this.setState(state => ({
+      weight_list: [...state.weight_list, state.weight_input + " kg " + date],
+      weight_input: ""
+    }));
+  };
+
+  delete = index => {
+    this.setState(state => {
+      const weight_list = [...state.weight_list];
+      weight_list.splice(index, 1);
+      return {
+        weight_list: weight_list
+      };
+    });
+  };
 }
 
 export default App;
